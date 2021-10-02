39265a0bc52ac59d74848325df94ab66ee912414
Andrea1141 <luiero96@gmail.com>
2021-10-02 17:43:48 +0200

    Updated program

--- hash calculator.py
+++ hash calculator.py
@@ -1,32 +1,34 @@
 import tkinter, hashlib
 
 root = tkinter.Tk()
-root.title("Hash Program")
+root.title("Hash Calculator")
 
 label = tkinter.Label(text="Write the string to hash")
 label.pack()
 
-string = tkinter.StringVar()
 option = tkinter.StringVar()
-option.set("sha256")
+option.set("sha224")
+string = tkinter.StringVar()
 
-entry = tkinter.Entry(root, textvariable=string)
+entry = tkinter.Entry(root, textvariable=string, width=150, justify="center")
 entry.pack()
 
 hexdigest = tkinter.StringVar()
-label = tkinter.Entry(text="", textvariable=hexdigest, width=70, justify="center", state="readonly")
+label = tkinter.Entry(text="", textvariable=hexdigest, width=150, justify="center", state="readonly")
 label.pack()
 
-def callback(_, __, ___):
+def callback(*args):
     encoded_string = string.get().encode()
     command = "hashlib." + option.get() + "(encoded_string)"
     result = eval(command)
     hexdigest.set(result.hexdigest())
-    
+
 string.trace_add("write", callback)
 option.trace_add("write", callback)
 
-menu = tkinter.OptionMenu(root, option, "sha256", "md5", "sha1")
+algorithms = ["sha224", "sha1", "blake2s", "sha3_384", "sha256", "blake2b", "sha384", "sha3_256", "sha3_512", "md5", "sha512", "sha3_224"]
+menu = tkinter.OptionMenu(root, option, *algorithms)
 menu.pack()
+callback()
 
 root.mainloop()
